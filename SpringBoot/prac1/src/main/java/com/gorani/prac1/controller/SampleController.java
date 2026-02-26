6b9cd47457649b0e2dd534a8aa1579c82b3f61c1
Yejin <leeyjin0926@gmail.com>
2026-02-26 10:53:59 +0900

    init SpringBoot

---
+++ SpringBoot/prac1/src/main/java/com/gorani/prac1/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.gorani.prac1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/hello")
+    public String[] hello() {
+        return new String[]{"Hello", "World"};
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye World";
+    }
+}
---
+++ SpringBoot/prac1/src/test/java/com/gorani/prac1/Prac1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gorani.prac1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prac1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("[TEST] contextLoads....");
+    }
+
+}
