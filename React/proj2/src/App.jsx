4c24642bcf913b44b6bc8db7acbea4acde55da5c
Yejin <leeyjin0926@gmail.com>
2026-03-17 15:03:51 +0900

    react: useReducer 이해하기

--- React/proj2/src/App.jsx
+++ React/proj2/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "../component/Header";
 import TodoEditor from "../component/TodoEditor";
 import TodoList from "../component/TodoList";
 import { useRef, useState } from "react";
+import TestComp from "../component/TestComp";
 
 function App() {
   const [todo, setTodo] = useState(mockTodo);
@@ -42,6 +43,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <TestComp />
       <TodoEditor onCreate={onCreate} />
       <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
